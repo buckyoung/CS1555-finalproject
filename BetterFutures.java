d940da71e6c53e3308d5156627cae2bf47726435
professir <buckcyoung@gmail.com>
2014-12-07 11:25:04 -0500

    allocations implemented

--- BetterFutures.java
+++ BetterFutures.java
@@ -21,7 +21,7 @@ public class BetterFutures {
 		try{
 			DriverManager.registerDriver ( new oracle.jdbc.driver.OracleDriver() );
 			String url = "jdbc:oracle:thin:@class3.cs.pitt.edu:1521:dbclass"; 
-			connection = DriverManager.getConnection(url, "bcy3", "3471045"); 
+			connection = DriverManager.getConnection(url, "bcy3", "xxxxxx"); 
 		}
 		catch( Exception e ) { System.out.println("Error connecting to database.  Machine Error: " + e.toString() ); }
 		/*finally	{ 
